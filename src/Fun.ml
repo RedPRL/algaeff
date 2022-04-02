c382cf0cec612a81803a565afb1497f824811bfd
favonia <favonia@gmail.com>
2022-04-02 11:51:38 -0500

    style(Fun)!: finally, use "finally"

--- src/Fun.ml
+++ src/Fun.ml
@@ -2,12 +2,12 @@ open StdlibShim
 
 module Deep =
 struct
-  let retinue k f =
+  let finally k f =
     match f () with
     | x -> Effect.Deep.continue k x
     | exception e -> Effect.Deep.discontinue k e
 
-  let retinue_preserving_backtrace k f =
+  let finally_preserving_backtrace k f =
     match f () with
     | x -> Effect.Deep.continue k x
     | exception e ->
@@ -17,12 +17,12 @@ end
 
 module Shallow =
 struct
-  let retinue_with k f h =
+  let finally_with k f h =
     match f () with
     | x -> Effect.Shallow.continue_with k x h
     | exception e -> Effect.Shallow.discontinue_with k e h
 
-  let retinue_preserving_backtrace_with k f h =
+  let finally_preserving_backtrace_with k f h =
     match f () with
     | x -> Effect.Shallow.continue_with k x h
     | exception e ->
