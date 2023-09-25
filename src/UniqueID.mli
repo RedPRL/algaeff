4fd66526de51dfd700c981f4b9dbb0f7b68f459b
favonia <favonia@gmail.com>
2023-09-25 06:07:38 -0500

    docs: tweaks (#23)

--- src/UniqueID.mli
+++ src/UniqueID.mli
@@ -34,7 +34,7 @@ sig
     (** Printing the ID. *)
 
     val unsafe_of_int : int -> t
-    (** Unsafe conversion from {!type:int}. Should be used only for de-serialization. *)
+    (** Unsafe conversion from [int]. Should be used only for de-serialization. *)
   end
 
   type id = ID.t
@@ -56,7 +56,7 @@ sig
   *)
 
   val register_printer : ([`Register of elt | `Retrieve of id | `Export] -> string option) -> unit
-  (** [register_printer p] registers a printer [p] via {!val:Printexc.register_printer} to convert unhandled internal effects into strings for the OCaml runtime system to display. Ideally, all internal effects should have been handled by {!val:run} and there is no need to use this function, but when it is not the case, this function can be helpful for debugging. The functor {!module:Modifier.Make} always registers a simple printer to suggest using {!val:run}, but you can register new ones to override it. The return type of the printer [p] should return [Some s] where [s] is the resulting string, or [None] if it chooses not to convert a particular effect. The registered printers are tried in reverse order until one of them returns [Some s] for some [s]; that is, the last registered printer is tried first. Note that this function is a wrapper of {!val:Printexc.register_printer} and all the registered printers (via this function or {!val:Printexc.register_printer}) are put into the same list.
+  (** [register_printer p] registers a printer [p] via {!val:Printexc.register_printer} to convert unhandled internal effects into strings for the OCaml runtime system to display. Ideally, all internal effects should have been handled by {!val:run} and there is no need to use this function, but when it is not the case, this function can be helpful for debugging. The functor {!module:UniqueID.Make} always registers a simple printer to suggest using {!val:run}, but you can register new ones to override it. The return type of the printer [p] should return [Some s] where [s] is the resulting string, or [None] if it chooses not to convert a particular effect. The registered printers are tried in reverse order until one of them returns [Some s] for some [s]; that is, the last registered printer is tried first. Note that this function is a wrapper of {!val:Printexc.register_printer} and all the registered printers (via this function or {!val:Printexc.register_printer}) are put into the same list.
 
       The input type of the printer [p] is a variant representation of the internal effects used in this module. They correspond to the effects trigger by {!val:register}, {!val:retrieve} and {!val:export}. More precisely,
       - [`Register elt] corresponds to the effect triggered by [register elt].
